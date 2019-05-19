175bda74da7820096ddf4b17144c7c6f7ebf40af
caiquefv <vitorianocaique@gmail.com>
2019-05-19 14:31:28 -0300

    criando interface DAO generica

--- src/main/java/br/edu/ifpb/domain/Banda.java
+++ src/main/java/br/edu/ifpb/domain/Banda.java
@@ -5,10 +5,97 @@
  */
 package br.edu.ifpb.domain;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
  * @author caique
  */
 public class Banda {
+    private int id;
+    private String localDeOrigem;
+    private String nomeFantasia;
+    private List<Integrante> integrantes;
+
+    public Banda() {
+    }
+
+    public Banda(int id, String localDeOrigem, String nomeFantasia, List<Integrante> integrantes) {
+        this.id = id;
+        this.localDeOrigem = localDeOrigem;
+        this.nomeFantasia = nomeFantasia;
+        this.integrantes = integrantes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocalDeOrigem() {
+        return localDeOrigem;
+    }
+
+    public void setLocalDeOrigem(String localDeOrigem) {
+        this.localDeOrigem = localDeOrigem;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public List<Integrante> getIntegrantes() {
+        return integrantes;
+    }
+
+    public void setIntegrantes(List<Integrante> integrantes) {
+        this.integrantes = integrantes;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.id;
+        hash = 59 * hash + Objects.hashCode(this.localDeOrigem);
+        hash = 59 * hash + Objects.hashCode(this.nomeFantasia);
+        hash = 59 * hash + Objects.hashCode(this.integrantes);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Banda other = (Banda) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.localDeOrigem, other.localDeOrigem)) {
+            return false;
+        }
+        if (!Objects.equals(this.nomeFantasia, other.nomeFantasia)) {
+            return false;
+        }
+        if (!Objects.equals(this.integrantes, other.integrantes)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
--- src/main/java/br/edu/ifpb/domain/Integrante.java
+++ src/main/java/br/edu/ifpb/domain/Integrante.java
@@ -5,10 +5,96 @@
  */
 package br.edu.ifpb.domain;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 /**
  *
  * @author caique
  */
 public class Integrante {
+    private int id;
+    private String nome;
+    private LocalDate dataDeNascimento;
+    private CPF cpf = new CPF("");
+
+    public Integrante() {
+    }
+
+    public Integrante(int id, String nome, LocalDate dataDeNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(CPF cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + this.id;
+        hash = 67 * hash + Objects.hashCode(this.nome);
+        hash = 67 * hash + Objects.hashCode(this.dataDeNascimento);
+        hash = 67 * hash + Objects.hashCode(this.cpf);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Integrante other = (Integrante) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.dataDeNascimento, other.dataDeNascimento)) {
+            return false;
+        }
+        if (!Objects.equals(this.cpf, other.cpf)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
